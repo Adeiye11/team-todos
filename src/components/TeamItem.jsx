85f5ac51a0dc8f0bf1e78bc11852b90d64c2e7fd
EstherAdegbaju <princessadeiye@gmail.com>
2023-09-20 13:28:57 +0200

    Update TeamItem.jsx

--- src/components/TeamItem.jsx
+++ src/components/TeamItem.jsx
@@ -1,28 +1,25 @@
 import React from "react";
-import { Avatar, Button, Container, Stack, Typography, Divider } from "@mui/material";
+import { Avatar, Button, Container, Stack, Typography } from "@mui/material";
 
 function TeamItem({ teamMember, onclick }) {
     return (
         <>
-            <Stack direction={"row"} onClick={() => onclick(teamMember)}>
-                <Container>
-                    <Avatar src={`https://picsum.photos/seed/${teamMember.id}/500/300`} />
-                </Container>
-                <Stack direction={"row"} flexWrap={"wrap"} gap={0.5} minWidth={0}>
+            <Stack direction={"row"} onClick={() => onclick(teamMember)} sx={{ minWidth: 0 }} p={2}>
+                <Avatar src={`https://picsum.photos/seed/${teamMember.id}/500/300`} />
+                <Stack direction={"row"}>
                     <Container>
-                        <Typography variant={"h6"}>
+                        <Typography variant={"body1"}>
                             {teamMember.name}
                         </Typography>
                         <Typography variant={"body2"}>
                             {teamMember.email}
                         </Typography>
                     </Container>
-                    <Button variant={'outlined'}>
-                        View Todos
+                    <Button variant={'contained'}>
+                        Todos
                     </Button>
                 </Stack>
             </Stack>
-            <Divider />
         </>
     );
 }
