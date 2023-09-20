c6320a8c0af196d6ff8529de7ff16a5c1af41b7d
EstherAdegbaju <princessadeiye@gmail.com>
2023-09-20 14:50:01 +0200

    Update TeamItem.jsx

--- src/components/TeamItem.jsx
+++ src/components/TeamItem.jsx
@@ -6,19 +6,21 @@ function TeamItem({ teamMember, onclick }) {
         <>
             <Stack direction={"row"} onClick={() => onclick(teamMember)} sx={{ minWidth: 0 }} p={2}>
                 <Avatar src={`https://picsum.photos/seed/${teamMember.id}/500/300`} />
-                <Stack direction={"row"}>
-                    <Container>
-                        <Typography variant={"body1"}>
-                            {teamMember.name}
-                        </Typography>
-                        <Typography variant={"body2"}>
-                            {teamMember.email}
-                        </Typography>
-                    </Container>
-                    <Button variant={'contained'}>
-                        Todos
-                    </Button>
-                </Stack>
+                <Container>
+                    <Stack direction={"row"}>
+                        <Container>
+                            <Typography variant={"body1"}>
+                                {teamMember.name}
+                            </Typography>
+                            <Typography variant={"body2"}>
+                                {teamMember.email}
+                            </Typography>
+                        </Container>
+                        <Button variant={'outlined'} size={"small"} >
+                            Todos
+                        </Button>
+                    </Stack>
+                </Container>
             </Stack>
         </>
     );
