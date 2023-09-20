bfd914f69cd03d7543e376d45487dc34c8bbc398
EstherAdegbaju <princessadeiye@gmail.com>
2023-09-20 15:32:08 +0200

    Update TeamMemberDetail.jsx

--- src/components/TeamMemberDetail.jsx
+++ src/components/TeamMemberDetail.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import { Typography, Avatar, Container, Stack } from "@mui/material";
+import { Typography, Avatar, Stack } from "@mui/material";
 
 
 function TeamMemberDetail({ teamMember }) {
     return (
-        <Stack sx={{ padding: 10, margin: "auto", alignItems: "center", justifyContent: "center", display: "flex" }}>
+        <Stack sx={{ minWidth: 0, padding: 10, margin: "auto", alignItems: "center", justifyContent: "center", display: "flex" }}>
             <Avatar src={`https://picsum.photos/seed/${teamMember.id}/500/300`} sx={{ width: 150, height: 150 }} />
             <Typography variant={"h4"} sx={{ padding: 2 }}>
                 {teamMember.name}
