1b8104306364e58c9b9e45d1525f2a463e2e69c4
EstherAdegbaju <princessadeiye@gmail.com>
2023-09-20 15:20:22 +0200

    Add Search

--- src/components/TeamList.jsx
+++ src/components/TeamList.jsx
@@ -1,11 +1,19 @@
-import React from "react";
-import { Stack, Divider } from "@mui/material";
+import React, { useState } from "react";
+import { Stack, Divider, TextField } from "@mui/material";
 import TeamItem from "./TeamItem";
 
 function TeamList({ team, onclick }) {
+    const [filteredTeam, setFilteredTeam] = useState(team);
+
+    const handleTextChange = (text) => {
+        const newTeam = team.filter((teamMember) => teamMember.name.toLowerCase().includes(text.toLowerCase()));
+        setFilteredTeam(newTeam);
+    };
     return (
         <Stack divider={<Divider />} p={3} bgcolor={"#f7f7f7"} borderRight={"solid 1px #e3e3e3"}>
-            {team.map((teamMember) => (
+            {/* search input */}
+            <TextField id="outlined-basic" label="Search" variant="outlined" sx={{ background: "white" }} margin={"normal"} onChange={(event) => handleTextChange(event.target.value)} />
+            {filteredTeam.map((teamMember) => (
                 <TeamItem key={teamMember.id} teamMember={teamMember} onclick={onclick} />
             ))}
         </Stack>
